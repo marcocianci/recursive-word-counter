77a4f6c50b940215f0e82bb841cb89d254576ee4
Marco Cianci <marco@cianci.com.br>
2017-04-02 01:14:47 -0300

    dev

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # recursive-word-counter
+This component returns the total words of a text.
+
 PHP: Recursive Word Counter | Contador de Palavras Recursivo
+
+## Installation
+
+You can add this library as a local, per-project dependency to your project using [Composer](https://getcomposer.org/):
+
+    composer require marcocianci/word-counter
+    
+If you only need this library during development, for instance to run your project's test suite, then you should add it as a development-time dependency:
+
+    composer require --dev marcocianci/word-counter
+    
+## Usage
+
+```php
+<?php
+use MarcoCianci\WordCounter;
+
+$wordCounter = new WordCounter;
+$text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi cursus varius felis, ut hendrerit magna semper varius. Fusce sed consequat.';
+$wordsTotal = $wordCounter->wordCounter($text);
+
+
+```
--- index.php
+++ index.php
@@ -1,56 +1,9 @@
+<?php
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Recursive Word Counter | Contador de Palavras Recursivo</title>
-</head>
-<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-<script>
-$(document).ready(function() {
-  $( "#btn_send" ).on( "click", function(event) {
-    var $form = $( '#counterForm' ),$textos2 = $form.find( "textarea[name='textos[]']" ).val();
-    var sendTxt = { textos: $textos2 };
-	  $.ajax({
-        type: "POST",
-        url: './class.conta_palavras.php',
-        dataType: "json",
-        data: sendTxt,
-        success: function (data) {
-        if (data) {
-          if (data.erro) {
-            alert(data.erro);
-            return;
-          }
-          $ulli = '<ul>';
-          $.each(data.PalavrasUnicas, function(key, value) {
-            $ulli += '<li>'+key+' : '+value+'</li>';
-          });
-          $ulli += '</ul>';
-          $( "#result" ).empty().html('Total de Palavras: '+ data.TotalPalavras +'<br />'+ 'Palavras únicas: <br />' + $ulli);
-        } else {
-          alert("erro!");
-        }
-      }
-    });
-  });
-});
-</script>
-<body>
-<div id="content">
-	<form id="counterForm" action="" method="POST">
-	  <fieldset>
-	  <legend>Contador de Palavras</legend>
-	    <label for="name">Digite aqui suas palavras e/ou frases:</label>
-	    <textarea rows="4" cols="50" name="textos[]"></textarea>
-	    <br />
-	    <center>
-	    	<button id="btn_send" class="btn" type="button">calcular</button>
-	    </center>
-	    <br />
-	    <div id="result"></div>
-	  </fieldset>
-	</form>
-</div>
-</body>
-</html>
+require_once __DIR__ . '/vendor/autoload.php'; // Autoload files using Composer autoload
+
+use MarcoCianci\WordCounter;
+
+$wordCounter = new WordCounter;
+$text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi cursus varius felis, ut hendrerit magna semper varius. Fusce sed consequat.';
+$wordsTotal = $wordCounter->wordCounter($text);
